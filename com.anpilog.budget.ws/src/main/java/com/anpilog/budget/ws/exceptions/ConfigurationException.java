e835ad68c04561fb944c4b117d244f156924b3b1
Andrei Anpilogov <aianitro@gmail.com>
2018-08-25 11:42:31 -0700

    ConfigurationException extended to take snapshots

--- com.anpilog.budget.ws/src/main/java/com/anpilog/budget/ws/exceptions/ConfigurationException.java
+++ com.anpilog.budget.ws/src/main/java/com/anpilog/budget/ws/exceptions/ConfigurationException.java
@@ -1,11 +1,18 @@
 package com.anpilog.budget.ws.exceptions;
 
+import com.anpilog.budget.ws.core.UberWebDriver;
+
 public class ConfigurationException extends Exception {
 
 	private static final long serialVersionUID = -7554191299889226373L;
 
 	public ConfigurationException(String message) {
 		super(message);
+	}
+
+	public ConfigurationException(String message, UberWebDriver webdriver) {
+		super(message);
+		webdriver.takeScreenshot();
 	}
 
 }
